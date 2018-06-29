9774d0afc84d4713bbfdd48c0ed7c36597e7a233
nukeop <alsw@protonmail.com>
2018-06-30 01:39:28 +0200

    Add symbol font

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ rm -rf demo
 sass --scss znaki.scss znaki.css
 sass --scss demo.scss demo.css
 mkdir demo
-cp Drogowskaz_lekki_owalny.ttf demo
 cp znaki.css demo
 cp demo.css demo
-cp font.css demo
 cp index.html demo
+cp -r fonts demo
